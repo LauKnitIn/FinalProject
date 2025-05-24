1f991afeab228f0206dd8ebd35dd7d5a96947470
wjhf007 <wj007hf@gmail.com>
2025-05-24 11:55:47 -0500

    front

---
+++ src/main/java/co/edu/uptc/presenter/Presenter.java
@@ -0,0 +1,15 @@
+package co.edu.uptc.presenter;
+
+import co.edu.uptc.view.View;
+
+public class Presenter {
+    private View view;
+
+    public Presenter(View view) {
+        this.view = view;
+    }
+
+    public void showPanelStart() {
+        view.showPanelStart();
+    }
+}
---
+++ src/main/java/co/edu/uptc/presenter/Runner.java
@@ -0,0 +1,11 @@
+package co.edu.uptc.presenter;
+
+import co.edu.uptc.view.View;
+
+public class Runner {
+    public static void main(String[] args) {
+        View view = new View(null);
+        Presenter presenter = new Presenter(view);
+        view.setVisible(true);
+    }
+}
