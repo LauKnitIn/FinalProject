8d7e6e225abadf562f3e3a4829c14d5509bf4828
wjhf007 <wj007hf@gmail.com>
2025-05-24 00:50:57 -0500

    primer commit

--- src/main/java/co/edu/uptc/model/WordHandler.java
+++ src/main/java/co/edu/uptc/model/WordHandler.java
@@ -2,9 +2,11 @@ package co.edu.uptc.model;
 
 import java.io.BufferedInputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class WordHandler {
 
@@ -17,12 +19,11 @@ public class WordHandler {
         this.gson = new Gson();
         this.reader = new InputStreamReader(this.getClass().getResourceAsStream(this.PATH));
         this.wordListTest = this.gson.fromJson(this.reader, new TypeToken <Map<String,List<String>>>(){}.getType());
-        this.wordListTest.
+        // this.wordListTest.
     }
 
     public void showWord(){
-     this.wordListTest.
+    //  this.wordListTest.
     }
 
 }
-lpiouihguy
