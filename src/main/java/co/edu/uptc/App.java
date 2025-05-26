9d2186dfa8a305c463ac56de6d63157accae882a
Sofia <correabsofial@gmail.com>
2025-05-25 22:51:04 -0500

    OnePLayer and Multiplayer game logic UNTESTED

--- src/main/java/co/edu/uptc/App.java
+++ src/main/java/co/edu/uptc/App.java
@@ -1,16 +1,10 @@
 package co.edu.uptc;
 
-import co.edu.uptc.model.Word;
-import co.edu.uptc.model.WordHandler;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        WordHandler lol = new WordHandler();
+        
     }
 }
--- src/main/java/co/edu/uptc/model/Player.java
+++ src/main/java/co/edu/uptc/model/Player.java
@@ -4,19 +4,66 @@ public class Player {
 
     private String name;
     private Word wordToGuess;
+    private boolean isWinner;
     private int remainingAttempts;
 
 
-    public Player(String name, Word wordToGuess){
-        this.name = name;
-        this.wordToGuess = wordToGuess;
+    public Player(int maxAttempts){
+        this.remainingAttempts = maxAttempts;
+        this.isWinner = false;
     }
 
     public boolean guessLetter(char letter){
-        return wordToGuess.hasLetter(letter);
+        boolean hasGuessed = wordToGuess.hasLetter(letter);
+        if (!(hasGuessed)) {
+            this.remainingAttempts--;
+        }
+        return hasGuessed;
     }
 
     public boolean hasWon(){
         return wordToGuess.isComplete();
     }
+    public boolean hasLost(){
+        boolean hasAttempts = true;
+        if (remainingAttempts <= 0) {
+            hasAttempts = false;
+        }
+        return hasAttempts;
+    }
+
+    public void chooseWord(Word desiredWord, Player oppPlayer){
+        if (desiredWord != null && oppPlayer != null) {
+            oppPlayer.setWordToGuess(desiredWord);    
+        }
+    }
+
+    public Word getWordToGuess() {
+        return wordToGuess;
+    }
+
+    public void setWordToGuess(Word wordToGuess) {
+        this.wordToGuess = wordToGuess;
+
+    }
+
+    public int getRemainingAttempts(){
+        return remainingAttempts;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public boolean isWinner() {
+        return isWinner;
+    } 
+
+    public void setIsWinner(boolean isWinner) {
+        this.isWinner = isWinner;
+    }
+    
 }
