74d342256b713a76f91e81a14948dccbf5c89c4d
Juan Pablo Martinez <juan.martinez53@uptc.edu.co>
2025-05-26 15:19:50 -0500

    Panel de ingraso nombre responsivo

--- src/main/java/co/edu/uptc/view/PanelLoginName.java
+++ src/main/java/co/edu/uptc/view/PanelLoginName.java
@@ -6,13 +6,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.text.AbstractDocument;
-import javax.swing.text.DocumentFilter;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
 import co.edu.uptc.view.constants.ColorPalette;
 import co.edu.uptc.view.constants.FontPalette;
@@ -20,9 +20,13 @@ import co.edu.uptc.view.constants.FontPalette;
 public class PanelLoginName extends JPanel {
 
     private Image backgroundImage;
+    private JLabel titleLabel;
+    private RoundedTextField nameTextField;
+    private RoundedButton continuarButton;
 
     public PanelLoginName() {
-        setSize(1300, 800);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width, screenSize.height);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
         backgroundImage = new ImageIcon("resources\\FondoMenus.png").getImage();
         setLayout(null);
@@ -41,13 +45,11 @@ public class PanelLoginName extends JPanel {
     }
 
     private void addTitle() {
-        int frameWidth = 1300;
         int labelWidth = 1000;
         int labelHeight = 100;
-        int xPosition = (frameWidth - labelWidth) / 2;
-
-        JLabel title = createLabel("Ingrese su nombre", xPosition, 150, labelWidth, labelHeight);
-        add(title);
+        int yPosition = 150;
+        titleLabel = createLabel("Ingrese su nombre", 0, yPosition, labelWidth, labelHeight);
+        add(titleLabel);
     }
 
     private JLabel createLabel(String text, int x, int y, int width, int height) {
@@ -59,50 +61,33 @@ public class PanelLoginName extends JPanel {
     }
 
     private void addOptions() {
-        addTextField("___________________", 330, 350, e -> {
-        });
+    nameTextField = addTextField("  _______________________________  ", 0, 350, e -> {
+    });
 
-        addButton("Continuar", 480, 500, e -> showPanelDifficulty());
-    }
+    continuarButton = addButton("Continuar", 0, 500, e -> showPanelDifficulty());
+}
 
-    private void addButton(String text, int x, int y, ActionListener actionListener) {
-        RoundedButton button = new RoundedButton(text, 20);
-        button.setBounds(x, y, 370, 70);
-        button.setBackground(ColorPalette.COLOR_BUTTON);
-        button.setForeground(Color.WHITE);
-        button.setFont(FontPalette.BUTTON_2_FONT);
-        button.addActionListener(actionListener);
-        add(button);
-    }
+private RoundedButton addButton(String text, int x, int y, ActionListener actionListener) {
+    RoundedButton button = new RoundedButton(text, 20);
+    button.setBounds(x, y, 370, 70);
+    button.setBackground(ColorPalette.COLOR_BUTTON);
+    button.setForeground(Color.WHITE);
+    button.setFont(FontPalette.BUTTON_2_FONT);
+    button.addActionListener(actionListener);
+    add(button);
+    return button;
+}
 
-    private void addTextField(String text, int x, int y, ActionListener actionListener) {
-        RoundedTextField textField = new RoundedTextField(1, 30, 30, text);
-        textField.setBounds(x, y, 650, 90);
-        textField.setFont(FontPalette.TEXTFIELD_FONT);
-        textField.setBackground(ColorPalette.COLOR_TEXTFIELD);
-        textField.setForeground(Color.WHITE);
-        textField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+private RoundedTextField addTextField(String text, int x, int y, ActionListener actionListener) {
+    RoundedTextField textField = new RoundedTextField(1, 30, 30, text);
+    textField.setBounds(x, y, 650, 70);
+    textField.setFont(FontPalette.TEXTFIELD_FONT);
+    textField.setBackground(ColorPalette.COLOR_TEXTFIELD);
+    textField.setForeground(Color.WHITE);
+    add(textField);
+    return textField;
+}
 
-        ((AbstractDocument) textField.getDocument()).setDocumentFilter(new DocumentFilter() {
-            @Override
-            public void insertString(FilterBypass fb, int offset, String string, javax.swing.text.AttributeSet attr)
-                    throws javax.swing.text.BadLocationException {
-                if (fb.getDocument().getLength() + string.length() <= 22) {
-                    super.insertString(fb, offset, string, attr);
-                }
-            }
-
-            @Override
-            public void replace(FilterBypass fb, int offset, int length, String text,
-                    javax.swing.text.AttributeSet attrs) throws javax.swing.text.BadLocationException {
-                if (fb.getDocument().getLength() - length + (text != null ? text.length() : 0) <= 22) {
-                    super.replace(fb, offset, length, text, attrs);
-                }
-            }
-        });
-
-        add(textField);
-    }
 
     public void showPanelDifficulty() {
         JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(PanelLoginName.this);
@@ -110,5 +95,34 @@ public class PanelLoginName extends JPanel {
             ((View) topFrame).showPanelDifficulty();
         }
     }
+
+    @Override
+public void doLayout() {
+    super.doLayout();
+    if (titleLabel != null) {
+        int panelWidth = getWidth();
+        int labelWidth = titleLabel.getWidth();
+        int yPosition = titleLabel.getY();
+        int labelHeight = titleLabel.getHeight();
+        int xPosition = (panelWidth - labelWidth) / 2;
+        titleLabel.setBounds(xPosition, yPosition, labelWidth, labelHeight);
+    }
+    if (nameTextField != null) {
+        int panelWidth = getWidth();
+        int fieldWidth = nameTextField.getWidth();
+        int yPosition = nameTextField.getY();
+        int fieldHeight = nameTextField.getHeight();
+        int xPosition = (panelWidth - fieldWidth) / 2;
+        nameTextField.setBounds(xPosition, yPosition, fieldWidth, fieldHeight);
+    }
+    if (continuarButton != null) {
+        int panelWidth = getWidth();
+        int btnWidth = continuarButton.getWidth();
+        int yPosition = continuarButton.getY();
+        int btnHeight = continuarButton.getHeight();
+        int xPosition = (panelWidth - btnWidth) / 2;
+        continuarButton.setBounds(xPosition, yPosition, btnWidth, btnHeight);
+    }
+}
 
 }
