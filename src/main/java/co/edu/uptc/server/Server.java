0854b5d5a4d7f48026acdf3bd4af110614acc5a3
Sofia <correabsofial@gmail.com>
2025-05-27 18:59:18 -0500

    Server logic separated

--- src/main/java/co/edu/uptc/server/Server.java
+++ src/main/java/co/edu/uptc/server/Server.java
@@ -55,43 +55,6 @@ public class Server {
             System.err.println("Error al detener el servidor: " + e.getMessage());
         }
     }
-    /*public void init() {
-        startServer();
-    }*/
-
-
-    /*public void startServer() {
-        try {
-            server = new ServerSocket(PORT);
-            System.out.println("Servidor iniciado");
-            while (true) {
-                System.out.println("Ha entrado al while");
-                Socket sc = server.accept();
-                nClient++;
-                System.out.println(nClient);
-                System.out.println("Cliente: " + nClient + " conectado");
-                ClientManager cm = new ClientManager(sc, nClient, this);
-                connectedClients.add(cm);
-                cm.start();
-                if (!cm.isAlive()) {
-                    System.out.println(cm.isAlive());
-                    System.out.println(">>>Client " + nClient + "disconnected<<<");
-                }
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-
-    public static String showActiveClientsList() {
-        String clients = "[ ";
-        for (ClientManager cliente : connectedClients) {
-            clients+=(cliente.isAlive())?("(" +cliente.nClient + ") "): "";
-        }
-        clients+=("]");
-        return clients;
-    }*/
-    
 
     public static void main(String[] args) {
         Server server = new Server();
--- src/main/java/co/edu/uptc/view/constants/Locations.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uptc.view.constants;
-
-import java.net.URL;
-
-public class Locations {
-    public static URL menuBg = Locations.class.getResource("../../imagesResources/FondoMenus.png");
-    public static URL infoIcon = Locations.class.getResource("../../imagesResources/informacion.png");
-}
